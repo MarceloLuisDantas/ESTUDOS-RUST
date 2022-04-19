9be5e7f2105c6685204124866150a7a45498c506
Marcelo Luis <marceloluis257gh@gmail.com>
2022-04-19 02:03:58 -0300

    Adicionado umas coisa

---
+++ Algoritmos/Ordenação/quick_sort/target/debug/deps/quick_sort-a9d9d7593477332d.d
@@ -0,0 +1,5 @@
+/home/marcelo/Documents/codigos/Rust/Estudos-Rust/Algoritmos/Ordenação/quick_sort/target/debug/deps/quick_sort-a9d9d7593477332d: src/main.rs
+
+/home/marcelo/Documents/codigos/Rust/Estudos-Rust/Algoritmos/Ordenação/quick_sort/target/debug/deps/quick_sort-a9d9d7593477332d.d: src/main.rs
+
+src/main.rs:
